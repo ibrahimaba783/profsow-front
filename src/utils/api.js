e66c8069950a43dd3260a015dbccd1340ae21a40
Ibrahima Ba <ibrahimaba783@gmail.com>
2026-07-08 14:12:48 +0000

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://profsow-back.onrender.com/api';
 
 const api = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
